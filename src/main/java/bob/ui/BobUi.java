8b21ab2a9f2f528b5308be2264181d3a1cec57dd
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-11 17:00:13 +0200

    käyttöliittymän muotoilua

--- src/main/java/bob/ui/BobUi.java
+++ src/main/java/bob/ui/BobUi.java
@@ -1,27 +1,49 @@
-
 package bob.ui;
 
-import java.util.HashSet;
-import java.util.Set;
+
+
+import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.*;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.canvas.*;
+import javafx.scene.layout.*;
 
+public class BobUi extends Application {
 
-public class BobUi extends Application{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(BobUi.class);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //tänne vielä: designii! meil on datapisteet mut sivunlukeminen falskaa
+        //sit kans kysbäriihän on onks tää milläi taval hyvä tapa tehä asioita :))))
         primaryStage.setTitle("Dis Is BOB");
-        FlowPane komponenttiryhma = new FlowPane();
-        Scene scene = new Scene(komponenttiryhma);
-        primaryStage.setMinHeight(300);
+        BorderPane components = new BorderPane();
+
+        Canvas topheader = new Canvas(650, 250);
+        GraphicsContext drawTop = topheader.getGraphicsContext2D();
+        drawTop.setFill(Color.BLACK);
+
+        Canvas foot = new Canvas(650, 80);
+        GraphicsContext drawBottom = foot.getGraphicsContext2D();
+        drawBottom.setFill(Color.BLACK);
+        
+        VBox textElements = new VBox();
+        textElements.setPadding(new Insets(180));
+        textElements.getChildren().add(new Label("MUISTUTUKSET"));
+
+        components.setTop(topheader);
+        components.setCenter(textElements);
+        components.setBottom(foot);
+
+        Scene scene = new Scene(components);
+        primaryStage.setMinHeight(880);
+        primaryStage.setMinWidth(680);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
-
