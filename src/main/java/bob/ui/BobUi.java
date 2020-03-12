089603211df661923944f8cc29745944457a3a40
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-12 16:12:51 +0200

    yksinkertaistettiin

--- src/main/java/bob/ui/BobUi.java
+++ src/main/java/bob/ui/BobUi.java
@@ -1,14 +1,10 @@
 package bob.ui;
 
-import javafx.scene.paint.Color;
-import javafx.scene.control.Label;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.canvas.*;
-import javafx.scene.layout.*;
+
 
 public class BobUi extends Application {
 
@@ -18,37 +14,11 @@ public class BobUi extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //tänne vielä: designii! meil on datapisteet mut sivunlukeminen falskaa
-        //sit kans kysbäriihän on onks tää milläi taval hyvä tapa tehä asioita :))))
-//        primaryStage.setTitle("Dis Is BOB");
-//        BorderPane components = new BorderPane();
-//
-//        Canvas topheader = new Canvas(650, 250);
-//        GraphicsContext drawTop = topheader.getGraphicsContext2D();
-//        drawTop.setFill(Color.BLACK);
-//
-//        Canvas foot = new Canvas(650, 80);
-//        GraphicsContext drawBottom = foot.getGraphicsContext2D();
-//        drawBottom.setFill(Color.BLACK);
-//        
-//        VBox textElements = new VBox();
-//        textElements.setPadding(new Insets(180));
-//        textElements.getChildren().add(new Label("MUISTUTUKSET"));
-//
-//        components.setTop(topheader);
-//        components.setCenter(textElements);
-//        components.setBottom(foot);
-//
-//        Scene scene = new Scene(components);
-//        primaryStage.setMinHeight(880);
-//        primaryStage.setMinWidth(680);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-        Parent root = FXMLLoader.load(getClass().getResource("/testfxml.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/scenebuildbob.fxml"));
 
         Scene scene = new Scene(root);
 
-        primaryStage.setTitle("JavaFX and Maven");
+        primaryStage.setTitle("bob the personal assistant <3");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
