f646a04ceaeef91cfb463e85428997ec5c2b7670
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-18 16:28:20 +0200

    testifxml

--- src/main/java/bob/BobUi.java
+++ src/main/java/bob/BobUi.java
@@ -1,6 +1,9 @@
 package bob;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class BobUi extends Application {
@@ -11,6 +14,11 @@ public class BobUi extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader sceneLoader = new FXMLLoader(getClass().getResource("/kakkaa.fxml"));
+        Parent root = sceneLoader.load();
+        Scene primaryScene = new Scene(root);
+        
+        primaryStage.setScene(primaryScene);
         primaryStage.setTitle("bob the personal assistant <3");
         primaryStage.show();
     }
