e9aec963708774c2eb1b6d0bbb83a052145909b3
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-18 17:05:10 +0200

    luotiin controllerit

---
+++ src/main/java/bob/PrimarySceneController.java
@@ -0,0 +1,10 @@
+package bob;
+
+public class PrimarySceneController {
+    private BobUi application;
+    
+    public void setApplication(BobUi application){
+        this.application = application;
+    }
+    
+}
