e02fdfcb6eaa1fe19ee8118f6acc4d737c1b820e
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-19 08:41:19 +0200

    näkymien toiminnallisuuksia

--- src/main/java/bob/BobUi.java
+++ src/main/java/bob/BobUi.java
@@ -16,10 +16,14 @@ public class BobUi extends Application {
     public void init() throws Exception{
         FXMLLoader primarySceneLoader = new FXMLLoader(getClass().getResource("/fxml/primaryScene.fxml"));
         Parent primaryRoot = primarySceneLoader.load();
+        PrimarySceneController primarySceneController = primarySceneLoader.getController();
+        primarySceneController.setApplication(this);
         primaryScene = new Scene(primaryRoot);
         
         FXMLLoader newReminderSceneLoader = new FXMLLoader(getClass().getResource("/fxml/newReminderScene.fxml"));
         Parent newReminderSceneRoot = newReminderSceneLoader.load();
+        NewReminderSceneController newReminderSceneController = newReminderSceneLoader.getController();
+        newReminderSceneController.setApplication(this);
         newReminderScene = new Scene(newReminderSceneRoot);
     }
 
@@ -29,6 +33,14 @@ public class BobUi extends Application {
         primaryStage.setScene(primaryScene);
         primaryStage.setTitle("bob the personal assistant <3");
         stage.show();
+    }
+    
+    public void setPrimaryScene(){
+        stage.setScene(primaryScene);
+    }
+    
+    public void setNewReminderScene(){
+        stage.setScene(newReminderScene);
     }
     
     public static void main(String[] args) {
--- src/main/java/bob/PrimarySceneController.java
+++ src/main/java/bob/PrimarySceneController.java
@@ -1,10 +1,29 @@
 package bob;
 
-public class PrimarySceneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class PrimarySceneController implements Initializable{
     private BobUi application;
     
     public void setApplication(BobUi application){
         this.application = application;
+    }
+    
+    @FXML
+    private MenuItem lisääMuistutus;
+    
+    @FXML
+    private void handleSetNewReminderScene(){
+        application.setNewReminderScene();
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // skip
     }
     
 }
