fd20d0f18685d4dac4d44c246fcabc09935a86be
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-20 13:56:45 +0200

    metodeja muistutuksen luomista varten

---
+++ src/main/java/bob/domain/Reminder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bob.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author riikoro
+ */
+public class Reminder{
+    private String date;
+    private String description;
+
+    public Reminder(String date, String description) {
+        this.date = date;
+        this.description = description;
+    }
+
+    
+    
+    
+}
