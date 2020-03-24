5c0d53ff0ab7e8ea321ed5ecf34a91c94f9ce419
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-24 16:38:45 +0200

    luotiin testiluokat

---
+++ src/test/java/ReminderTest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+
+public class ReminderTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    /*
+    testaa:
+    konstruktori: reminder!=null, d=d, date=date
+    getterit
+    */
+}
