14d4045da2600f3737fa96e430a4663cb991d2d3
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-27 12:56:45 +0200

    testit daoon ja reminderiin. kattavuus: catchit puuttuu

---
+++ src/test/java/BobServiceTest.java
@@ -0,0 +1,25 @@
+import bob.domain.BobService;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BobServiceTest {
+    
+    private BobService bobService;
+    
+    @Before
+    public void setUp() {
+    }
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    //atm: ei testitarvetta; ainoot viitteet daoon
+}
