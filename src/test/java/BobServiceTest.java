4bb8d1b07b444f6d6001c1978b746748a7a201d4
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-27 13:07:35 +0200

    bobservicen testi

--- src/test/java/BobServiceTest.java
+++ src/test/java/BobServiceTest.java
@@ -1,19 +1,26 @@
+import bob.dao.BobDao;
 import bob.domain.BobService;
-import org.junit.After;
-import org.junit.AfterClass;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class BobServiceTest {
     
     private BobService bobService;
+    private BobDao bobDao;
     
     @Before
     public void setUp() {
+        this.bobDao = new BobDao();
+        this.bobService = new BobService(bobDao);
     }
     
+    @Test
+    public void bobServiceExists(){
+        assertThat(bobService, is(notNullValue()));
+    }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
