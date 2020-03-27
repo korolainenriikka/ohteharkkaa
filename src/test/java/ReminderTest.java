8dd2be07cd9b8df51efc4ae58b98edff3b9528c4
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-27 12:19:29 +0200

    reminderTest

--- src/test/java/ReminderTest.java
+++ src/test/java/ReminderTest.java
@@ -1,25 +1,32 @@
-import org.junit.After;
+import bob.domain.Reminder;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Before;
+import org.junit.Test;
 
 public class ReminderTest {
     
+    private Reminder reminder;
+    
     @Before
     public void setUp() {
+        this.reminder = new Reminder("2020-03-27", "tämä on testimuistutus");
     }
     
-    @After
-    public void tearDown() {
+    @Test
+    public void reminderExists(){
+        assertThat(reminder, is(notNullValue()));
+    }
+    
+    @Test
+    public void getDateWorks(){
+        assertThat(reminder.getDate(), equalTo("2020-03-27"));
+    }
+    
+    @Test
+    public void getDescriptionWorks(){
+        assertThat(reminder.getDescription(), equalTo("tämä on testimuistutus"));
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-    
-    /*
-    testaa:
-    konstruktori: reminder!=null, d=d, date=date
-    getterit
-    */
+
 }
