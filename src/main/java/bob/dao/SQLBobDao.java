f740f265bac4683d7b83118ae17f333d35d47aa2
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-04-09 15:23:11 +0300

    db table rename

--- src/main/java/bob/dao/SQLBobDao.java
+++ src/main/java/bob/dao/SQLBobDao.java
@@ -12,78 +12,95 @@ public class SQLBobDao implements BobDao {
     public SQLBobDao(String database) {
         try {
             connection = DriverManager.getConnection(database);
-            Statement s = connection.createStatement();
-            s.execute("CREATE TABLE IF NOT EXISTS Muistutukset(id INTEGER PRIMARY KEY, date DATE, description TEXT);");
-            s.execute("CREATE TABLE IF NOT EXISTS Tapahtumat(id INTEGER PRIMARY KEY, date DATE, time TIME, description TEXT);");
         } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
+            System.err.println(e);
 
+        }
+        createDatabase();
     }
 
-    public String addReminderToDatabase(Reminder newReminder) {
+    private void createDatabase() {
         try {
-            PreparedStatement stmt = connection.prepareStatement("INSERT INTO Muistutukset(date, description) VALUES (?,?)");
+            Statement s = connection.createStatement();
+            s.execute("CREATE TABLE IF NOT EXISTS Reminders(id INTEGER PRIMARY KEY, date DATE, description TEXT);");
+            s.execute("CREATE TABLE IF NOT EXISTS Events(id INTEGER PRIMARY KEY, date DATE, time TIME, description TEXT);");
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+
+    public boolean addReminderToDatabase(Reminder newReminder) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO Reminders(date, description) VALUES (?,?)");
             stmt.setString(1, newReminder.getDate() + "");
             stmt.setString(2, newReminder.getDescription());
             stmt.executeUpdate();
-            return ("uusi muistutus lisätty:\n" + newReminder.getDate() + "\n" + newReminder.getDescription());
-        } catch (SQLException e) {
-            return e.getMessage();
-        }
-    }
-
-    public List<CalendarItem> getTodaysCalendarItems(LocalDate today) {
-        ArrayList<CalendarItem> todaysItems = new ArrayList<>();
-        try {
-            
-            PreparedStatement stmt2 = connection.prepareStatement("SELECT * FROM Tapahtumat WHERE date=(?);");
-            stmt2.setString(1, today + "");
-            ResultSet r2 = stmt2.executeQuery();
-            while (r2.next()) {
-                todaysItems.add(new Event(LocalDate.parse(r2.getString("date")), LocalTime.parse(r2.getString("time")), r2.getString("description")));
-            } 
-            todaysItems.add(new Reminder(LocalDate.now(), "*"));
-            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Muistutukset WHERE date=(?);");
-            stmt.setString(1, today + "");
-            ResultSet r = stmt.executeQuery();
-            while (r.next()) {
-                todaysItems.add(new Reminder(LocalDate.parse(r.getString("date")), r.getString("description")));
-            }        
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-
-        return todaysItems;
-    }
-
-    @Override
-    public boolean removeOld(LocalDate today) {
-        try {
-            PreparedStatement stmtReminder = connection.prepareStatement("DELETE FROM Muistutukset WHERE date < (?)");
-            PreparedStatement stmtEvent = connection.prepareStatement("DELETE FROM Tapahtumat WHERE date < (?)");
-            stmtReminder.setString(1, today + "");
-            stmtEvent.setString(1, today + "");
-            stmtReminder.executeUpdate();
-            stmtEvent.executeUpdate();
             return true;
         } catch (SQLException e) {
-            System.err.println(e.getMessage());
             return false;
         }
     }
 
-    public String addEventToDatabase(Event newEvent) {
+    public boolean addEventToDatabase(Event newEvent) {
         try {
-            PreparedStatement stmt = connection.prepareStatement("INSERT INTO Tapahtumat(date, time, description) VALUES (?,?,?)");
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO Events(date, time, description) VALUES (?,?,?)");
             stmt.setString(1, newEvent.getDate() + "");
             stmt.setString(2, newEvent.getTime() + "");
             stmt.setString(3, newEvent.getDescription());
             stmt.executeUpdate();
-            return ("uusi tapahtuma lisätty:\n" + newEvent.getDate() + "\n" + newEvent.getTime() + "\n" + newEvent.getDescription());
+            return true;
         } catch (SQLException e) {
-            return e.getMessage();
+            return false;
         }
     }
 
+    @Override
+    public boolean removeOld(LocalDate today) {
+        String[] calendarItemTables = {"Reminders", "Events"};
+        try {
+            PreparedStatement stmt = connection.prepareStatement("DELETE FROM ? WHERE date < (?)");
+            for (int i = 0; i < calendarItemTables.length; i++) {
+                stmt.setString(1, calendarItemTables[i]);
+                stmt.setString(2, today + "");
+                stmt.executeUpdate();
+            }
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public List<CalendarItem> getTodaysEvents(LocalDate today) {
+        List<CalendarItem> todaysEvents = new ArrayList<>();
+        PreparedStatement stmt2;
+        try {
+            stmt2 = connection.prepareStatement("SELECT * FROM Events WHERE date=(?);");
+            stmt2.setString(1, today + "");
+            ResultSet r2 = stmt2.executeQuery();
+            while (r2.next()) {
+                todaysEvents.add(new Event(LocalDate.parse(r2.getString("date")), LocalTime.parse(r2.getString("time")), r2.getString("description")));
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return todaysEvents;
+    }
+
+    @Override
+    public List<CalendarItem> getTodaysReminders(LocalDate today) {
+        List<CalendarItem> todaysReminders = new ArrayList<>();
+        PreparedStatement stmt2;
+        try {
+            stmt2 = connection.prepareStatement("SELECT * FROM Reminders WHERE date=(?);");
+            stmt2.setString(1, today + "");
+            ResultSet r2 = stmt2.executeQuery();
+            while (r2.next()) {
+                todaysReminders.add(new Reminder(LocalDate.parse(r2.getString("date")), r2.getString("description")));
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return todaysReminders;
+    }
 }
