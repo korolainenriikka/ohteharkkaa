cea6ec48008a002a26cffd373151b5dfcc388345
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-04-16 14:22:03 +0300

    muistutukset siirtyy seuraavalle päivälle

--- src/main/java/bob/EndDaySceneController.java
+++ src/main/java/bob/EndDaySceneController.java
@@ -4,51 +4,75 @@ import bob.domain.*;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
-public class EndDaySceneController implements SceneController{
-    
+public class EndDaySceneController implements SceneController {
+
     private BobUi application;
     private BobService bobService;
-    
+
     @Override
     public void setAttributes(BobUi application, BobService bobService) {
         this.application = application;
         this.bobService = bobService;
     }
-    
+
     @FXML
     private VBox todaysReminders;
-    
+
+    @FXML
+    private Button save;
+
+    private HashMap<CheckBox, Label> reminders;
+
     @FXML
     private void handleSetPrimaryScene(ActionEvent event) {
         application.setPrimaryScene();
     }
-    
+
+    @FXML
+    private void saveSelections() {
+        for (CheckBox cb : reminders.keySet()) {
+            if (!cb.isSelected()) {
+                //undone reminder is added to next day
+                bobService.moveReminderToNextDay(reminders.get(cb).getText());
+            } 
+        }
+        application.setPrimaryScene();
+    }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
     }
 
     public void setSceneContent(LocalDate today) {
+        todaysReminders.getChildren().clear();
+        reminders = new HashMap<>();
+        addRemindersToScene();
+    }
+
+    private void addRemindersToScene() {
         List<String> todaysRemindersAsString = bobService.getTodaysItemsAsString(Reminder.class);
-        List<HBox> reminders = new ArrayList<>();
-        if(!todaysRemindersAsString.isEmpty()){
+        List<HBox> remindersAsHBox = new ArrayList<>();
+        if (!todaysRemindersAsString.isEmpty()) {
             HBox header = new HBox();
-            header.getChildren().add(new Label("\nMUISTA!"));
-            reminders.add(header);
+            header.getChildren().add(new Label("\nValitse suoritetut:"));
+            remindersAsHBox.add(header);
         }
         for (String reminder : todaysRemindersAsString) {
             HBox hb = new HBox();
             hb.setSpacing(10);
-            hb.getChildren().addAll(new CheckBox(), new Label(reminder.toString()));
-            reminders.add(hb);
+            CheckBox checker = new CheckBox();
+            Label label= new Label(reminder.toString());
+            reminders.put(checker, label);
+            hb.getChildren().addAll(checker, label);
+            remindersAsHBox.add(hb);
         }
-        todaysReminders.getChildren().addAll(reminders);
+        todaysReminders.getChildren().addAll(remindersAsHBox);
     }
-    
+
 }
