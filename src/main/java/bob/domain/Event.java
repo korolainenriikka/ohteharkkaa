b7b051dc84bf72200e013fc4ca9a4ea5f7241213
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-05-09 13:40:11 +0300

    fixes

---
+++ src/main/java/bob/domain/Event.java
@@ -0,0 +1,45 @@
+package bob.domain;
+
+import java.time.*;
+
+/**
+ * Kalenteritapahtumaa kuvaava luokka, aika-parametri ei pakollinen
+ */
+public class Event implements CalendarItem {
+
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+
+    public Event(LocalDate date, LocalTime time, String description) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Metodi palauttaa kalenteritapahtuman merkkijonoesityksenä.
+     *
+     * @return merkkijonoesitys
+     */
+    @Override
+    public String toString() {
+        if (this.time == null) {
+            return this.description;
+        } else {
+            return "klo " + this.time + ": " + this.description;
+        }
+    }
+}
